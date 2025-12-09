63e0384099d602f667be42c28707127835ac6bc0
Stephan Grigorescu <stephan.grigorescu@gmail.com>
2025-12-09 17:45:03 +0100

    Modified to tight layout

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ from src.inference import create_predictor
 # PAGE CONFIGURATION
 # ============================================================================
 st.set_page_config(
-    layout="wide",
+    layout="tight",
     initial_sidebar_state="expanded"
 )
 
